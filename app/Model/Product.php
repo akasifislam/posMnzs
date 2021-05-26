c0b7809f3e70d372be3b7abaa108a7be7269f9c2
akasifislam <aseasifislam@gmail.com>
2021-05-26 23:20:47 +0600

    product management

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $guarded = ['id'];
+
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'id');
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit_id', 'id');
+    }
 }
--- resources/views/backend/product/view-product.blade.php
+++ resources/views/backend/product/view-product.blade.php
@@ -13,14 +13,14 @@
                       </h3>
                   </div>
                   <div class="card-body">
-                    <table id="example1" class="table table-sm table-bordered table-striped">
+                    <table id="example1" class="table table-sm table-hover table-bordered table-striped">
                         <thead>
                         <tr>
                           <th>Sl</th>
                           <th>Supplier Name</th>
-                          <th>Email</th>
-                          <th>Address</th>
-                          <th>Mobile No</th>
+                          <th>Category Name</th>
+                          <th>Supply Name</th>
+                          <th>Unit Key</th>
                           <th>Action</th>
                         </tr>
                         </thead>
@@ -29,9 +29,9 @@
                             <tr class="{{ $products->id }}">
                                 <td>{{ $key+1 }}</td>
                                 <td>{{ $products->name }}</td>
-                                <td>{{ $products->email }}</td>
-                                <td>{{ $products->address }}</td>
-                                <td>{{ $products->mobile_no }}</td>
+                                <td>{{ $products['category']['name'] }}</td>
+                                <td>{{ $products['supplier']['name'] }}</td>
+                                <td>{{ $products['unit']['name'] }}</td>
                                 <td>
                                   <div class="btn-group" role="group" aria-label="Basic example">
                                       <a href="{{ route('products.edit',$products->id) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
