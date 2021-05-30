67a0b30f808f8a6165811174db3f822df18b72c0
akasifislam <aseasifislam@gmail.com>
2021-05-30 23:47:25 +0600

    datePicker

--- resources/views/backend/purchase/add-purchase.blade.php
+++ resources/views/backend/purchase/add-purchase.blade.php
@@ -277,18 +277,18 @@
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
-        $(document).ready(function() {
-            $( "#datepicker" ).datepicker({
-                showButtonPanel: true,
-                changeMonth: true,
-                dateFormat: 'yy-mm-dd'
-            });
-        });
+        // $(document).ready(function() {
+        //     $( "#datepicker" ).datepicker({
+        //         showButtonPanel: true,
+        //         changeMonth: true,
+        //         dateFormat: 'yy-mm-dd'
+        //     });
+        // });
         // $(function(){
         //     $("#datepicker").datepicker();
         // });
-        // $(function () {
-        //     $('#datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
-        // });
+        $(function () {
+            $('#datepicker').datepicker({ format: 'dd/mm/yyyy' });
+        });
     </script>
 @endsection
